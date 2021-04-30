f7c2248e01f8770377a45750bf4b942af153c0c5
andoroth <david.p.goodman1@gmail.com>
2021-04-30 11:41:27 -0400

    Added compartment label to python model

--- buildModel.py
+++ buildModel.py
@@ -1,9 +1,32 @@
 import neuron,pprint,csv,os
+import numpy as np
 from neuron import h,rxd,gui
 from neuron.units import ms,mV
+from matplotlib import pyplot
 
 #mM,ms,mV
-print(neuron.__version__)
+#print(neuron.__version__)
+
+compartmentLabelMap = []
+with open('compartmentLabelMap.csv') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',',quotechar = '|')
+    
+    for row in spamreader:
+        compartmentLabelMap.append(row[0])
+
+compartmentLabelMap.insert(0,'soma')
+labelSet = set(compartmentLabelMap)
+compartmentIdx2LabelMap = np.array(compartmentLabelMap)
+
+compartmentLabel2IdxMap = {}
+for label in labelSet:
+    labelMatchIdxList = [i for i, x in enumerate(compartmentIdx2LabelMap) if x == label]
+    # print(label)
+    # print(labelMatchIdxList)
+    # print(compartmentLabelMap[labelMatchIdxList])
+    compartmentLabel2IdxMap[label] = labelMatchIdxList;
+    # print(compartmentLabel2IdxMap[label])    
+
 
 connectionVector = []
 with open('connectivityVector.csv') as csvfile:
@@ -45,21 +68,59 @@ for n in range(len(connectionVector)):
 sl = h.SectionList(sectionList)
 
 for sec in sl:
-    sec.insert('hh')
     sec.insert('pas')
+    sec(0.5).pas.g = 1.2e-3
     
-sectionList[0].insert('nat')
-sectionList[0].insert('nap')
-sectionList[0].insert('k_sh')
-sectionList[0].insert('k_shab')
-sectionList[0].insert('k_shal')
-sectionList[0].insert('k_shaw')
+for sec in compartmentLabel2IdxMap['siz']:
+    # print(sec)
+    sectionList[sec].insert('nat')
+    sectionList[sec].insert('nap')
+    sectionList[sec].insert('k_shal')
+    sectionList[sec].insert('k_shaw')
+    sectionList[sec].insert('k_shab')
+    sectionList[sec].insert('k_sh')
+    
 
-stimobj = h.IClamp(sectionList[0](0.5))
-stimobj.delay = 200
-stimobj.dur = 500
-stimobj.amp = 500
+# sectionList[0].insert('nat')
+# sectionList[0].insert('nap')
+# sectionList[0].insert('k_sh')
+# sectionList[0].insert('k_shab')
+# sectionList[0].insert('k_shal')
+# sectionList[0].insert('k_shaw')
+
+somaSection = compartmentLabel2IdxMap['soma'][0]
+sizSection = compartmentLabel2IdxMap['siz'][0]
+
+stimobj = h.IClamp(sectionList[somaSection](0.5))
+stimobj.delay = 15
+stimobj.dur = 85
+stimobj.amp = 5
 stimobj.i = 0
+
+h.tstop = 120
+
+vVec = h.Vector()
+tVec = h.Vector()
+
+vVec.record(sectionList[sizSection](0.5)._ref_v)
+tVec.record(h._ref_t)
+
+sectionList[sizSection](0.5).k_shaw.gmax = 1.3e-3
+sectionList[sizSection](0.5).k_shal.gmax = 1.3e-3
+sectionList[sizSection](0.5).k_shab.gmax = 1.8e-3
+sectionList[sizSection](0.5).nat.gmax = 900e-3
+for g in range(2,9):
+    gVal = g*1.5e-3
+    print(gVal)
+    sectionList[sizSection](0.5).k_sh.gmax = gVal
+
+    h.run()
+
+    pyplot.figure(figsize=(8,4)) # Default figsize is (8,6)
+    pyplot.plot(tVec, vVec)
+    pyplot.xlabel('time (ms)')
+    pyplot.ylabel('mV')
+    pyplot.show()
 
 s=h.Shape()
 s.show(False)
